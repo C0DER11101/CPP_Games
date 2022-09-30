f0a6306cd53e76720992e3ad2fa00aac9c4171be
Priyanuj Bora <www.priyanuj.bora13@gmail.com>
2022-09-30 15:04:20 +0530

    The time() function in C++

---
+++ time_Function.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<stdlib.h>
+#include<time.h>
+
+using namespace std;
+
+int main(void)
+{
+	while(1)
+	{
+		system("clear");
+		cout<<time(NULL)<<flush;
+		system("sleep 1");
+		
+	}
+
+	return 0;
+}
