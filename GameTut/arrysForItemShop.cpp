6eef0a5a2ed1ae3ba0b42a70ebfd96adc972587a
Priyanuj Bora <while.alive.program@gmail.com>
2022-10-18 07:14:02 +0530

    Item Shop

--- GameTut/arrysForItemShop.cpp
+++ GameTut/arrysForItemShop.cpp
@@ -3,16 +3,34 @@
 
 using namespace std;
 
+const int numItems=6;
+const string shopItemNames[numItems]={"Boots", "Swords", "Helmets", "Kittens", "Poleaxes", "Leggings"}; // array of strings!!
+// constant variables and arrays can be made global!!
+
+void printInventory(int[numItems]);
+
 int main(void)
 {
-	string shopItemNames[4]={"Boots", "Sword", "Helmet", "Kitten"}; // array of strings!!
 	
+	int playerInventory[numItems]={0, 0, 0, 1, 0, 0};
 	cout<<"***Welcome to Item Shop!!\n";
 
-	for(int i=0; i<4; i++)
+	for(int i=0; i<numItems; i++)
 		cout<<shopItemNames[i]<<"\n";
 	cout<<"*****************\n";
+	printInventory(playerInventory);
 	cout<<"What would you like to buy?\n";
 
 	return 0;
+}
+
+void printInventory(int playerInventory[numItems])
+{
+	cout<<"You inventory is:\n";
+	for(int i=0; i<numItems; i++)
+	{
+		if(playerInventory[i]>0)
+			cout<<playerInventory[i]<<" x "<<shopItemNames[i]<<"\n";
+	}
+	cout<<"\n";
 }
