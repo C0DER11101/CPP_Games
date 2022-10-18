b828650b36d140cb59a246caf3283f6d22364d09
Priyanuj Bora <while.alive.program@gmail.com>
2022-10-18 20:43:25 +0530

    Board

--- GameTut/Board.cpp
+++ GameTut/Board.cpp
@@ -12,11 +12,20 @@ int main(void)
 		for(int j=0; j<10; j++)
 			board[i][j]='|';
 
+	cout<<" ";
+	for(int i=0; i<9; i++)
+		cout<<" "<<i;
+	cout<<"\n ";
+	for(int i=0; i<19; i++)
+		cout<<"-";
+	cout<<"\n";
+
 	for(int i=0; i<10; i++)
 	{
+		cout<<i;
 		for(int j=0; j<10; j++)
 			cout<<board[i][j]<<" ";
-		cout<<"\n";
+		cout<<"\n ";
 		for(int j=0; j<19; j++)
 			cout<<"-";
 		cout<<"\n";
