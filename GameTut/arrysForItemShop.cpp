b77c32070b95b48986349cfdbc3efa5f2876f6f3
Priyanuj Bora <while.alive.program@gmail.com>
2022-10-18 10:36:32 +0530

    Item Shop

--- GameTut/arrysForItemShop.cpp
+++ GameTut/arrysForItemShop.cpp
@@ -8,18 +8,21 @@ const string shopItemNames[numItems]={"Boots", "Swords", "Helmets", "Kittens", "
 // constant variables and arrays can be made global!!
 
 void printInventory(int[numItems]);
+void printShop();
+void BuyItems(int[numItems]);
 
 int main(void)
 {
+	bool Done=false;
 	
 	int playerInventory[numItems]={0, 0, 0, 1, 0, 0};
-	cout<<"***Welcome to Item Shop!!\n";
-
-	for(int i=0; i<numItems; i++)
-		cout<<shopItemNames[i]<<"\n";
-	cout<<"*****************\n";
-	printInventory(playerInventory);
-	cout<<"What would you like to buy?\n";
+	
+	while(!Done) // this is the "Game Loop"
+	{
+		printShop();
+		printInventory(playerInventory);
+		BuyItems(playerInventory);
+	}
 
 	return 0;
 }
@@ -33,4 +36,37 @@ void printInventory(int playerInventory[numItems])
 			cout<<playerInventory[i]<<" x "<<shopItemNames[i]<<"\n";
 	}
 	cout<<"\n";
+}
+
+void printShop()
+{
+	cout<<"***Welcome to Item Shop!!\n";
+
+	for(int i=0; i<numItems; i++)
+		cout<<i+1<<"."<<shopItemNames[i]<<"\n";
+	cout<<"*****************\n";
+}
+
+void BuyItems(int playerInventory[numItems])
+{
+	int input;
+	cout<<"What would you like to buy? Enter ["<<1<<"-"<<numItems<<"]"<<":";
+	cin>>input;
+
+	if(input<0)
+	{
+		cout<<"Visit again!!\n";
+		exit(0);
+	}
+
+	if(input-1>=0 && input-1<numItems)
+	{
+		playerInventory[input-1]++;
+	}
+
+	else
+	{
+		cout<<"\n\nUh-oh! That was not expected!!\n";
+		return;
+	}
 }
