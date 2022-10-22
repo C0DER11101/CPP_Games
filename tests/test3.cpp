87bb920ef309f5f4b7253cb51881dbb88799a251
Priyanuj Bora <while.alive.program@gmail.com>
2022-10-22 23:12:00 +0530

    Taking Immediate input from user along with a space between them

---
+++ tests/test3.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<iomanip>
+#include<conio.h>
+
+using namespace std;
+
+int main(void)
+{
+	int row, col;
+
+	char n;
+
+	while(n!='e')
+	{
+		n=_getche();
+		row=n-'0';
+		n=_getche();
+		n=_getche();
+		col=n-'0';
+
+		cout<<"\n\n";
+		cout<<"Row"<<setw(30)<<"Col"<<"\n";
+		cout<<row<<setw(30)<<col<<"\n";
+	}
+
+	return 0;
+}
